ec4ba05b7d3ebbc13cd1e2e85175922858480565
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-01-07 21:36:45 +0000

    conversion

---
+++ newFsh/commands/fun/binary.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: ['binary','bin'],
+  params: ['type (encode/decode)', true, 'text', true],
+  info: "Encode & Decode binary",
+  category: "fun",
+
+  async execute(message, arguments2, fsh) {
+    arguments2 = message.content.split(' ');
+    arguments2 = arguments2.slice(1, arguments2.length);
+    if (!arguments2[1]) {
+      message.reply('you must fill everything');
+      return;
+    }
+    let data = await fetch('https://api.fsh.plus/binary?type='+arguments2[0]+'&text='+arguments2.slice(1,arguments2.length).join('%20'));
+    try {
+      data = await data.json();
+    } catch (err) {
+      message.reply('could not do')
+      return;
+    }
+
+    message.channel.send(`result:
+\`\`\`
+${data.text || data.msg}
+\`\`\``)
+  }
+};
