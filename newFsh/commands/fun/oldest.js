cb6181ef587ac3c3cc501f1b3ce82777ad0d98ae
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-08 20:25:01 +0000

    H

--- newFsh/commands/fun/oldest.js
+++ newFsh/commands/fun/oldest.js
@@ -1,12 +1,24 @@
-/*let news = {};
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "oldest",
+  info: "Info on command",
+  category: "hidden",
+
+  async execute(message, arguments2, fsh) {
+    let news = {};
     message.guild.members.cache.forEach(async m => {
       news[m.user.id] = m.joinedAt;
     })
     let snew = [];
-for (var i in news) {
+    for (var i in news) {
     snew.push([i, news[i]]);
-}
+    }
 
-snew.sort(function(a, b) {
+    snew.sort(function(a, b) {
     return a[1] - b[1];
-});*/
+    });
+
+    message.reply(snew[0])
+  }
+};
