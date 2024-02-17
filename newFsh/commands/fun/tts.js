8b75af0a4b148fca8beb1084135cfc519a681a8e
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-02-17 15:34:47 +0000

    tts command

---
+++ newFsh/commands/fun/tts.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "tts",
+  params: ['lang', true, 'text', true],
+  info: "Info on command",
+  category: "hidden",
+
+  async execute(message, arguments2, fsh) {
+    let data = await fetch('https://api.fsh.plus/tts?lang='+(arguments2[0].length == 2 ? arguments2[0] : 'en')+'&text='+(arguments2[0].length == 2 ? arguments2.slice(1,arguments2.length).join(' ') : arguments2.join(' ')));
+    data = await data.json();
+    
+    var binaryString = atob(data.audio.split(',')[1]);
+
+    var byteArray = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        byteArray[i] = binaryString.charCodeAt(i);
+    }
+    
+    const attachment = new Discord.AttachmentBuilder(Buffer.from(byteArray), {name: 'tts.wav'});
+
+    message.channel.reply({
+      files: [attachment]
+    })
+  }
+};
