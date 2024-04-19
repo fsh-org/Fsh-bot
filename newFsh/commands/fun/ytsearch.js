15617b077cc0d2238b8aa29664c94fc5cb37af79
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-04-19 17:34:03 +0000

    YTSearch command change

--- newFsh/commands/fun/ytsearch.js
+++ newFsh/commands/fun/ytsearch.js
@@ -1,21 +1,15 @@
 const Discord = require("discord.js");
 
-const pure = function (text) {
-  return String(String(text).replaceAll(",", String("\\,"))).replaceAll(
-    '"',
-    String('\\"')
-  );
-};
-
 module.exports = {
   name: "ytsearch",
   params: ["query", true],
   info: "Search for a youtube video",
   category: "fun",
   async execute(message, arguments2, fsh) {
-    let dat = await fetch(`https://poopoo-api.vercel.app/api/youtube/video?search=${arguments2.join("%20")}`);
+    let dat = await fetch(`https://api.fsh.plus/ytsearch?query=${arguments2.join("%20")}`);
     dat = await dat.json();
-
+    dat = dat.videos;
+    
     let embed = new Discord.EmbedBuilder()
       .setTitle(`${fsh.emojis.youtube} YouTube search "${arguments2.join(" ")}"`)
       .setTimestamp()
@@ -26,64 +20,15 @@ module.exports = {
       })
       .setColor("#888888");
 
-    dat.data.results.forEach((element) => {
-      if (response.data.results.indexOf(element) < 5) {
-        embed.addFields({
-          name: `${pure(element.title)} || by: ${pure(element.by)} - ${new Date(Number(pure(element.length)) * 1000).getHours()}h ${new Date(Number(pure(element.length)) * 1000).getMinutes()}m ${new Date(Number(pure(element.length)) * 1000).getSeconds()}s`,
-          value: `${pure(element.description)}\n${pure(element.views)} views | ${pure(element.uploaded)}\nlink: ${pure(element.link)}`,
-        });
-      }
+    dat.slice(0,5).forEach((element) => {
+      embed.addFields({
+        name: `${element.title.replaceAll(/#[a-zA-Z0-9]{1,10}/g, '').trim()} || By: ${element.author.name} - ${element.timestamp}`,
+        value: `Views: ${element.views} | Uploaded: ${element.ago} | [Link](${element.url})\n${element.description.replaceAll(/#[a-zA-Z0-9]{1,10}/g, '').replaceAll(/(http|https):\/\/.{2,15}\..{2,6}[a-zA-Z0-9\/\-_:]+? /g, '').trim()}`,
+      });
     });
+    
     message.channel.send({
       embeds: [embed]
     });
-    /*
-    axios({
-      method: "get",
-      url: `https://poopoo-api.vercel.app/api/youtube/video?search=${arguments2.join(
-        " "
-      )}`,
-      headers: {},
-    })
-      .then(async (response) => {
-        let embed = new Discord.EmbedBuilder()
-          .setTitle(
-            `${fsh.emojis.youtube} YouTube search "${arguments2.join(" ")}"`
-          )
-          .setTimestamp()
-          .setFooter({ text: "V" + fsh.version })
-          .setAuthor({
-            name: message.author.username,
-            iconURL: message.author.displayAvatarURL({ format: "png" }),
-          })
-          .setColor("#888888");
-        response.data.results.forEach((element) => {
-          if (response.data.results.indexOf(element) < 5) {
-            embed.addFields({
-              name: `${pure(element.title)} || by: ${pure(
-                element.by
-              )} - ${new Date(
-                Number(pure(element.length)) * 1000
-              ).getHours()}h ${new Date(
-                Number(pure(element.length)) * 1000
-              ).getMinutes()}m ${new Date(
-                Number(pure(element.length)) * 1000
-              ).getSeconds()}s`,
-              value: `${pure(element.description)}\n${pure(
-                element.views
-              )} views | ${pure(element.uploaded)}\nlink: ${pure(
-                element.link
-              )}`,
-            });
-          }
-        });
-        message.channel.send({
-          embeds: [embed],
-        });
-      })
-      .catch(async (err) => {
-        message.channel.send("Error");
-        console.log(err);
-      });*/
   }
 };
